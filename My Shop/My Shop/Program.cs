185642923a6beafa44429d8119bdaec52a0c3ac7
morovati135 <mmorovati2233@gmail.com>
2024-12-09 13:22:04 +0330

    Add project nuget Pakages

--- My Shop/My Shop/Program.cs
+++ My Shop/My Shop/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Identity;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -12,6 +14,8 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseHttpsRedirection();
+    
     app.UseSwagger();
     app.UseSwaggerUI();
 }
