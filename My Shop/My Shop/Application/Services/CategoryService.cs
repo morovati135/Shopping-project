1f35aea71998d224a3822fe793ea064eea8fdbda
morovati135 <mmorovati2233@gmail.com>
2024-12-09 20:49:05 +0330

    Add order , category , product with dapper services

---
+++ My Shop/My Shop/Application/Services/CategoryService.cs
@@ -0,0 +1,24 @@
+﻿using My_Shop.Core.Models;
+using My_Shop.Infrastructure.Repositories;
+
+namespace My_Shop.Application.Services;
+
+public class CategoryService : ICategoryService
+{
+    private readonly ICategoryRepository _categoryRepository;
+
+    public CategoryService(ICategoryRepository categoryRepository)
+    {
+        _categoryRepository = categoryRepository;
+    }
+
+    public async Task<IEnumerable<Category>> GetAllCategoriesAsync()
+    {
+        return await _categoryRepository.GetAllCategoriesAsync();
+    }
+
+    public async Task AddCategoryAsync(Category category)
+    {
+        await _categoryRepository.AddCategoryAsync(category);
+    }
+}
---
+++ My Shop/My Shop/Application/Services/ICategoryService.cs
@@ -0,0 +1,9 @@
+﻿using My_Shop.Core.Models;
+
+namespace My_Shop.Application.Services;
+
+public interface ICategoryService
+{
+    Task<IEnumerable<Category>> GetAllCategoriesAsync();
+    Task AddCategoryAsync(Category category);
+}
