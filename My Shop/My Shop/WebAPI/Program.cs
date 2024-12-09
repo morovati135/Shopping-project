ba8b80eab1bd373d3ecfa41e08118e852cff072b
morovati135 <mmorovati2233@gmail.com>
2024-12-09 17:41:21 +0330

    Creat db context in project

--- My Shop/My Shop/WebAPI/Program.cs
+++ My Shop/My Shop/WebAPI/Program.cs
@@ -1,11 +1,16 @@
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using My_Shop.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+// add db context to program class
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
