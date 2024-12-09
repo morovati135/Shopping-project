5ac86d05142d38839c4d3960db5168c5e8ce9e77
morovati135 <mmorovati2233@gmail.com>
2024-12-09 20:59:21 +0330

    Add Category Controller

---
+++ My Shop/My Shop/WebAPI/Controllers/CategoryController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using My_Shop.Application.Services;
+using My_Shop.Core.Models;
+
+namespace My_Shop.WebAPI.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class CategoryController : ControllerBase
+{
+    private readonly ICategoryService _categoryService;
+
+    public CategoryController(ICategoryService categoryService)
+    {
+        _categoryService = categoryService;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllCategories()
+    {
+        var categories = await _categoryService.GetAllCategoriesAsync();
+        return Ok(categories);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> AddCategory(Category category)
+    {
+        await _categoryService.AddCategoryAsync(category);
+        return Ok(new { Message = "Category added successfully." });
+    }
+}
