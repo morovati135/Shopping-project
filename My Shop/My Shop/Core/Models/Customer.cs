7bbb76723ec81e4f19ec48212d6dcd3de106c881
morovati135 <mmorovati2233@gmail.com>
2024-12-09 17:05:48 +0330

    add Models to project

---
+++ My Shop/My Shop/Core/Models/Customer.cs
@@ -0,0 +1,9 @@
+﻿namespace My_Shop.Core.Models;
+
+public class Customer
+{
+    public int Id { get; set; }
+    public string Username { get; set; }
+    public string Password { get; set; }
+    public string JwtToken { get; set; }
+}
---
+++ My Shop/My Shop/Core/Models/Order.cs
@@ -0,0 +1,9 @@
+﻿namespace My_Shop.Core.Models;
+
+public class Order
+{
+    public int Id { get; set; }
+    public int CustomerId { get; set; }
+    public DateTime OrderDate { get; set; }
+    public bool IsFinalized { get; set; }
+}
