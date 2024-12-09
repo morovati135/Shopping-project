689a9d2db1df78e65c3db857e4a6c3ad1e65a907
morovati135 <mmorovati2233@gmail.com>
2024-12-09 20:49:05 +0330

    Add order , category services

--- My Shop/My Shop/WebAPI/Program.cs
+++ My Shop/My Shop/WebAPI/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using My_Shop.Application.Services;
 using My_Shop.Infrastructure;
+using My_Shop.Infrastructure.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,6 +14,13 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 //add customer service
 builder.Services.AddScoped<ICustomerService, CustomerService>();
+//add product service
+builder.Services.AddScoped<IProductService, ProductService>();
+//add order service
+builder.Services.AddScoped<IOrderService, OrderService>();
+//add category service
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+builder.Services.AddScoped<ICategoryService, CategoryService>();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
